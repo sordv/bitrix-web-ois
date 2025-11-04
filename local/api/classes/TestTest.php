176d37b4349cb341660392b1dc3e0daac6db4443
sordv <vladimirbaturin2@mail.ru>
2025-11-04 16:24:36 +0500

    try to fix "get"

--- local/api/classes/TestTest.php
+++ local/api/classes/TestTest.php
@@ -5,20 +5,31 @@ namespace Legacy\API;
 use Legacy\General\Constants;
 use Bitrix\Iblock\ElementTable;
 use Bitrix\Main\Loader;
+use Legacy\Iblock\IblockElementTable;
 
 class TestTest
 {
 	public static function get($arRequest)
 	{
-		Loader::includeModule('iblock');
+		if (Loader::includeModule('iblock')) { // чтобы работать с элементами модуля, необходимо его подключить, в примере работаем с инфоблоком
+			$query = IblockElementTable::query()
+				->withSelect()
+				->addFilter('IBLOCK_ID', Constants::IB_TESTTEST)
+				->withOrder(['SORT' => 'ASC'])
+				->withPage($arRequest['limit'], $arRequest['page'])
+			;
+			$count = $query->queryCountTotal(); // количество записей ответа
+			$db = $query->exec();
 
-		$params = [
-			'select' => ['*'],
-			'filter' => ['IBLOCK_ID' => Constants::IB_TESTTEST],
-		];
-
-		$items = ElementTable::getList($params)->fetchAll();
-
-		return $items;
+			$result = [];
+			while ($res = $db->fetch()) {
+				$result[] = $res;
+			}
+			return [
+				'count' => $count,
+				'items' => $result
+			];
+		}
+		throw new \Exception('Не удалось подключить необходимые модули');
 	}
 }
