2615b7280d545e643a41f522d71242bf21cf2c92
Max <fedonkin145@gmail.com>
2025-11-15 16:34:03 +0500

    add auth

---
+++ local/api/classes/Auth.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Legacy\API;
+
+use Legacy\General\Constants;
+use Bitrix\Iblock\ElementTable;
+use Bitrix\Main\Loader;
+
+class Auth
+{
+    public static function login($arRequest) {
+        $login = $arRequest['login'];
+        $password = $arRequest['password'];
+
+        global $USER;
+
+        $result = $USER->Login($login, $password, 'Y');
+
+        if (is_array($result) && $result['TYPE'] === 'ERROR') {
+            return [
+                'success' => false,
+                'error' => $result['MESSAGE']
+            ];
+        } else {
+            return [
+                'success' => true,
+                'userId' => $USER->GetID()
+            ];
+        }
+    }
+
+    public static function logout() {
+        global $USER;
+
+        $USER->Logout();
+    }
+}
